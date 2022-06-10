0f560e2b3cfbca4e2f95747064da7fda81d40b65
brunocriacoes <br.rafael@outlook.com>
2022-06-10 16:19:54 -0300

    fix

--- components/BannerHome/index.js
+++ components/BannerHome/index.js
@@ -5,17 +5,23 @@ export default function BannerHome() {
             <div className="flex items-center justify-items-start lg:h-screen w-full">
                 <div className="pl-5 lg:pl-20">
                     <h1 className="block text-9xl lg:text-[300px] text-Light-Orange font-Beastly font-normal">solo</h1>
-                    <div className='text-xs lg:text-lg text-Light-Orange font-TTHoves uppercase font-normal'>
+                    <div className='flex  text-xs lg:text-lg text-Light-Orange font-TTHoves uppercase font-normal'>
                         <span>
                             Substratos Premium. <br />
                             Plantas saudáveis.
                         </span>
-                        <span>
+                        <span className="hidden lg:block pl-[230px]">
                             Frutos saborosos. <br />
                             Colheita o ano todo.
                         </span>
                     </div>
-                    <h1 className="block text-9xl lg:text-[300px] text-Light-Orange font-Beastly font-normal">vivo</h1>
+                    <h1 className="block text-9xl lg:-mt-[45px] lg:text-[300px] text-Light-Orange font-Beastly font-normal">vivo</h1>
+                    <div className='flex lg:hidden  text-xs lg:text-lg text-Light-Orange font-TTHoves uppercase font-normal'>
+                        <span className="">
+                            Frutos saborosos. <br />
+                            Colheita o ano todo.
+                        </span>
+                    </div>
                 </div>
             </div>
             <ProdTop color="#225439" />
